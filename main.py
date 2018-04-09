4a343b06f4a786c2d2ffbd82c899353dc8e87b6d
JSKim <b2ird21@kaist.ac.kr>
2018-04-09 09:24:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-import sys
-sys.path.append("./lib/python")
-#import librosa
+import librosa
 import numpy as np
 import matplotlib.pyplot as plt
-import lib.mrcg.utils as util
-import scipy.io.wavfile
 
-audio_dir = './data/example/SNR103F3MIC021002_ch01.wav'
+import lib.matlab_py.utils as utils
 
-mode = 1
-th= 0.4
+audio_dir = './data/example/clean_speech.wav'
+
+mode = 3
+th = 0.4
+
 output_type = 1
 is_default = 1
-result = util.vad_func(audio_dir, mode, th, output_type, is_default)
-#s, audio_sr = librosa.load(audio_dir, sr=16000)
-#result = util.vad_func(audio_dir, mode, th, output_type)
-audio_sr, s = scipy.io.wavfile.read(audio_dir)
-print(len(s)/audio_sr)
-t_max = np.min((len(s),len(result[0])))
-t = np.arange(0.,t_max/16000,1/16000)
-s = np.divide(s,np.max(np.absolute(s)))
-result = np.multiply(result,0.3)
-plt.plot(t,s[0:t_max],'b')
-plt.plot(t,result[0][0:t_max],'g')
-plt.show()
+
+result = utils.vad_func(audio_dir, mode, th, output_type, is_default)
+s, audio_sr = librosa.load(audio_dir, sr=16000)
+
+t_max = np.minimum(s.shape[0], result.shape[0])
+
+t = np.arange(0, t_max)/audio_sr
+s = np.divide(s, np.max(np.absolute(s)))
+
+result = np.multiply(result, 0.3)
+plt.plot(t, s[0:t_max], 'b')
+plt.plot(t, result, 'g')
